60a8da92d8b9b3b8386ef5338d954a1a5a760b6d
mchalop2803 <116265015+mchalop2803@users.noreply.github.com>
2024-01-17 13:00:48 +0100

    Proyecto con carpeta CRUD, clase Conection, Script

---
+++ ProyectoDeInterfaces-main/README.md
@@ -0,0 +1,2 @@
+# ProyectoDeInterfaces
+Proyecto creado para grabajar en grupo e implementar diferentes funcionalidades
---
+++ ProyectoDeInterfaces-main/src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import vista.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo!");
+
+        VistaLogin vl = new VistaLogin();
+
+    }
+}
