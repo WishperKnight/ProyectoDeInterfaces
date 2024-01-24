e29d03cabbe1b2c6c8c2edef52a6e32a6111d838
WishperKnight <fcabagu0912@iescarrillo.es>
2024-01-24 02:50:37 +0100

    Codigo terminado :)
    Que alguien me eutanasie

--- src/main/java/org/example/src/modelo/UserAccountDAO.java
+++ src/main/java/org/example/src/modelo/UserAccountDAO.java
@@ -7,100 +7,115 @@ import java.sql.SQLException;
 
 import org.example.src.conector.Conection;
 import org.example.src.vista.VistaRegister;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
- * Clase que contiene los metodos necesarios para manejar al usuario en la bbdd
+ * Clase que contiene los métodos necesarios para manejar al usuario en la bbdd
  */
 public class UserAccountDAO {
-	private VistaRegister register = new VistaRegister();
+    private VistaRegister register = new VistaRegister();
 
-	/**
-	 * Inserta un Usuario en la bbdd según unos parametros dados en el registro
-	 * 
-	 * @param Useraccount
-	 * @throws SQLException
-	 */
-	public void insertarUser(UserAccount userAccount) throws SQLException {
-		Connection con = Conection.connection();
+    /**
+     * Inserta un Usuario en la bbdd según unos parámetros dados en el registro
+     * 
+     * @param userAccount
+     * @throws SQLException
+     */
+    public void insertarUser(UserAccount userAccount) throws SQLException {
+        Connection con = Conection.connection();
 
-		String query = "INSERT INTO Usuario (id_usuario, nombre, apellidos, nombre_usuario, contrasena, correo, telefono, direccion_envio) VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
-		try (PreparedStatement statement = con.prepareStatement(query)) {
-			statement.setString(1, userAccount.getName());
-			statement.setString(2, userAccount.getSurname());
-			statement.setString(3, userAccount.getUsername());
-			statement.setString(4, userAccount.getPassword());
-			statement.setString(5, userAccount.getEmail());
-			statement.setString(6, userAccount.getPhoneNumber());
-			statement.setString(7, userAccount.getAdress());
-			statement.executeUpdate();
-		}
+        // Hasheamos la contraseña antes de almacenarla en la base de datos
+        String hashedPassword = hashPassword(userAccount.getPassword());
 
-	}
+        String query = "INSERT INTO Usuario (id_usuario, nombre, apellidos, nombre_usuario, contrasena, correo, telefono, direccion_envio) VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = con.prepareStatement(query)) {
+            statement.setString(1, userAccount.getName());
+            statement.setString(2, userAccount.getSurname());
+            statement.setString(3, userAccount.getUsername());
+            statement.setString(4, hashedPassword);
+            statement.setString(5, userAccount.getEmail());
+            statement.setString(6, userAccount.getPhoneNumber());
+            statement.setString(7, userAccount.getAdress());
+            statement.executeUpdate();
+        }
+    }
 
-	/**
-	 * Metodo para validar el login de usuario
-	 * 
-	 * @param username nombre de cuenta prorporcionado por el usuario
-	 * @param password contrasena prorporcionada por el usuario
-	 * @return T si se ha realizado F si ha fallado
-	 */
-	public boolean validarUser(String username, String password) {
-		Connection con = Conection.connection();
-		boolean valido = false;
+    /**
+     * Método para validar el login de usuario
+     * 
+     * @param username nombre de cuenta proporcionado por el usuario
+     * @param password contraseña proporcionada por el usuario
+     * @return true si se ha realizado, false si ha fallado
+     */
+    public boolean validarUser(String username, String password) {
+        Connection con = Conection.connection();
+        boolean valido = false;
 
-		String query = "SELECT * FROM Usuario WHERE nombre_usuario=? AND contrasena=?;";
+        String query = "SELECT * FROM Usuario WHERE nombre_usuario=?;";
 
-		try (PreparedStatement statement = con.prepareStatement(query)) {
-			statement.setString(1, username);
-			statement.setString(2, password);
+        try (PreparedStatement statement = con.prepareStatement(query)) {
+            statement.setString(1, username);
 
-			try (ResultSet resultSet = statement.executeQuery()) {
-				valido = resultSet.next(); // Establecer valido como true si se encontró alguna fila
-			}
-		} catch (SQLException e) {
-			e.printStackTrace(); // Manejar la excepción adecuadamente
-		} finally {
-			// Cerrar la conexión aquí si es necesario
-		}
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Comparamos la contraseña ingresada con la almacenada en la base de datos
+                    String hashedPassword = resultSet.getString("contrasena");
+                    valido = BCrypt.checkpw(password, hashedPassword);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejar la excepción adecuadamente
+        } finally {
+            // Cerrar la conexión aquí si es necesario
+        }
 
-		return valido;
-	}
+        return valido;
+    }
 
-	/**
-	 * Metodo que comprueba si existe un ya un usuario con ese nombre en la bbdd
-	 * 
-	 * @param UserName
-	 * @return T si ya existe F si no existe
-	 */
-	public boolean comprobarUser(String userName) {
-	    boolean exist = false;
-	    Connection con = null;
+    /**
+     * Método que comprueba si ya existe un usuario con ese nombre en la bbdd
+     * 
+     * @param userName
+     * @return true si ya existe, false si no existe
+     */
+    public boolean comprobarUser(String userName) {
+        boolean exist = false;
+        Connection con = null;
 
-	    try {
-	        con = Conection.connection();
-	        String query = "SELECT * FROM Usuario WHERE nombre_usuario=?;";
+        try {
+            con = Conection.connection();
+            String query = "SELECT * FROM Usuario WHERE nombre_usuario=?;";
 
-	        try (PreparedStatement statement = con.prepareStatement(query)) {
-	            statement.setString(1, userName);
+            try (PreparedStatement statement = con.prepareStatement(query)) {
+                statement.setString(1, userName);
 
-	            try (ResultSet resultSet = statement.executeQuery()) {
-	                exist = resultSet.next(); // Establecer exist como true si se encontró alguna fila
-	            }
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace(); // Manejar la excepción adecuadamente
-	    } finally {
-	        // Cerrar la conexión aquí si es necesario
-	        try {
-	            if (con != null) {
-	                con.close();
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace(); // Manejar la excepción adecuadamente
-	        }
-	    }
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    exist = resultSet.next(); // Establecer exist como true si se encontró alguna fila
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejar la excepción adecuadamente
+        } finally {
+            // Cerrar la conexión aquí si es necesario
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace(); // Manejar la excepción adecuadamente
+            }
+        }
 
-	    return exist;
-	}
+        return exist;
+    }
 
+    /**
+     * Método para cifrar la contraseña utilizando BCrypt
+     * 
+     * @param password Contraseña a cifrar
+     * @return Contraseña cifrada
+     */
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
